f5df0398b12f4ffda3dc420ca6d90a639ce049c5
hoanghh2003 <hoangvv111@gmail.com>
2025-02-18 16:41:28 +0700

    update

--- FE/src/routers/Routers.jsx
+++ FE/src/routers/Routers.jsx
@@ -5,24 +5,22 @@ import MainLayout from "../pages/MainLayout/MainLayout";
 import AuthPage from "../pages/AuthPage/AuthPage";
 import ResetPassword from "../pages/AuthPage/resetPassword";
 import ManagerPage from "../pages/ManagerPage/ManagerPage";
-
-
+import PrivateRoute from "./PrivateRoute";
 
 export const router = createBrowserRouter([
   {
     path: PATH_NAME.HOME,
     element: (
-      // <PrivateRoute>
-      <MainLayout />
-      // </PrivateRoute>
+      <PrivateRoute>
+        <MainLayout />
+      </PrivateRoute>
     ),
     children: [
       // index: true
       { index: true, element: <HomePage /> },
-      {path:"/auth", element: <AuthPage/>},
-      {path:"/reset-password", element: <ResetPassword/>},
-      {path:"/manager-page", element: <ManagerPage/>},
+      { path: "/auth", element: <AuthPage /> },
+      { path: "/reset-password", element: <ResetPassword /> },
+      { path: "/manager-page", element: <ManagerPage /> },
     ],
   },
-
 ]);
