5d0a77854fa4682251131bd60b64205eb7d97f1b
voleducanh93 <voleducanh93@gmail.com>
2025-02-24 16:08:39 +0700

    Profile_Children

--- BE/ChildVaccineSystem/ChildVaccineSystem.Data/Entities/Children.cs
+++ BE/ChildVaccineSystem/ChildVaccineSystem.Data/Entities/Children.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChildVaccineSystem.Data.Enum;
 
 namespace ChildVaccineSystem.Data.Entities
 {
@@ -14,12 +15,19 @@ namespace ChildVaccineSystem.Data.Entities
         public int ChildId { get; set; }
         public string FullName { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string Gender { get; set; }
+
+        public Gender Gender { get; set; }
+
         public string MedicalHistory { get; set; }
-        public string RelationToUser { get; set; }
+        public RelationToUser RelationToUser { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string ImageUrl { get; set; }
 
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
     }
+
+
 }
