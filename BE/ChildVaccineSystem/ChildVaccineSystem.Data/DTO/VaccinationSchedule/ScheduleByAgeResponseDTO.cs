a94254a4354f14354012ae2484625a40a0d9e261
Son Tom <quangsonnguyen2006@gmail.com>
2025-02-25 20:36:55 +0700

    Add API for VaccinationSchedule

---
+++ BE/ChildVaccineSystem/ChildVaccineSystem.Data/DTO/VaccinationSchedule/ScheduleByAgeResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿using ChildVaccineSystem.Data.DTO.ComboVaccine;
+using ChildVaccineSystem.Data.DTO.Vaccine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildVaccineSystem.Data.DTO.VaccinationSchedule
+{
+	public class ScheduleByAgeResponseDTO
+	{
+		
+		public List<VaccineDTO> Vaccines { get; set; } = new List<VaccineDTO>();
+
+		public List<ComboVaccineDTO> ComboVaccines { get; set; } = new List<ComboVaccineDTO>();
+
+	}
+}
