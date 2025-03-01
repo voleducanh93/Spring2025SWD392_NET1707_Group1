e7b33ed3758bd649124bcad61680312157b4bf4b
voleducanh93 <voleducanh93@gmail.com>
2025-03-01 10:55:15 +0700

    GetAllDoctor

--- BE/ChildVaccineSystem/ChildVaccineSystem.Data/DTO/UserDTO.cs
+++ BE/ChildVaccineSystem/ChildVaccineSystem.Data/DTO/UserDTO.cs
@@ -16,5 +16,7 @@ namespace ChildVaccineSystem.Data.DTO
         public string Address { get; set; }
         public DateTime DateOfBirth { get; set; }
         public bool IsActive { get; set; }
+        public string Role { get; set; } // Thêm trường Role vào DTO
+
     }
 }
