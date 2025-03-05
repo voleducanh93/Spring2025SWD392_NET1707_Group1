34e17be8d74109d287f90c7dd832cc6d96f7ba4c
voleducanh93 <voleducanh93@gmail.com>
2025-03-05 11:04:12 +0700

    fix

--- BE/ChildVaccineSystem/ChildVaccineSystem.API/Controllers/AdminController.cs
+++ BE/ChildVaccineSystem/ChildVaccineSystem.API/Controllers/AdminController.cs
@@ -211,7 +211,7 @@ namespace ChildVaccineSystem.API.Controllers
         }
 
         [HttpGet("getAllDoctors")]
-        [Authorize(AuthenticationSchemes = "Bearer", Roles = "Admin, Staff")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = "Admin,Staff")]
         public async Task<IActionResult> GetAllDoctors()
         {
             try
@@ -255,7 +255,7 @@ namespace ChildVaccineSystem.API.Controllers
         }
 
         [HttpGet("getAllRoles")]
-        [Authorize(AuthenticationSchemes = "Bearer", Roles = "Admin")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = "Admin,Staff")]
         public async Task<IActionResult> GetAllRoles()
         {
             try
