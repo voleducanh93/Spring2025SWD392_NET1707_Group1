86ceeb49a244b2e10590b9842833d97d4d722e58
voleducanh93 <voleducanh93@gmail.com>
2025-03-16 13:10:38 +0700

    CRUD_vaccine_thêm thuộc tính

--- BE/ChildVaccineSystem/ChildVaccineSystem.Common/Helper/MappingProfile.cs
+++ BE/ChildVaccineSystem/ChildVaccineSystem.Common/Helper/MappingProfile.cs
@@ -46,10 +46,15 @@ namespace ChildVaccineSystem.Common.Helper
             // Vaccine Mapping
             CreateMap<Vaccine, VaccineDTO>().ReverseMap();
 
-            CreateMap<CreateVaccineDTO, Vaccine>();
+            CreateMap<CreateVaccineDTO, Vaccine>()
+                .ForMember(dest => dest.ParentVaccine, opt => opt.Ignore()) // ✅ Xử lý riêng trong service
+                .ForMember(dest => dest.IsIncompatibility, opt => opt.MapFrom(src => src.IsIncompatibility));
 
             CreateMap<UpdateVaccineDTO, Vaccine>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+                .ForMember(dest => dest.ParentVaccine, opt => opt.Ignore()) // ✅ Xử lý riêng trong service
+                .ForMember(dest => dest.IsIncompatibility, opt => opt.MapFrom(src => src.IsIncompatibility))
+                                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
 
             // ComboVaccine Mapping
             CreateMap<ComboVaccine, ComboVaccineDTO>()
