d4ee0d0cc735539802716fee9f47e58a4bedcfb3
Ha Huy Hoang <hoangvv111@gmail.com>
2025-03-20 23:43:05 +0700

    aa

--- FE/src/pages/HomePage/HomePage.jsx
+++ FE/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,16 @@
+import CarouselComponent from "../../components/homeCarousel/Blog";
 import HomeCarousel from "../../components/homeCarousel/HomeCarousel";
 import Cards from "../../components/ui/Cards";
+
 
 export default function HomePage() {
   return (
     <div>
       <HomeCarousel />
       <Cards />
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <CarouselComponent />
+    </div>
     </div>
   );
 }
