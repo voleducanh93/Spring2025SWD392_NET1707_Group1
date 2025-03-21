290ee1d9341a07565d77863eaec6174ccce70131
Ha Huy Hoang <hoangvv111@gmail.com>
2025-03-21 12:13:16 +0700

    fe

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,12 +1,13 @@
 // import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routers/Routers";
-
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
-
-    <RouterProvider router={router} fallbackElement={<></>}/>
+    <div><SpeedInsights />
+    <RouterProvider router={router} fallbackElement={<></>}/></div>
+    
   );
 }
 
